1d0b0548c656997361fc9a4f897368e3972a2b8a
salmalah <s.salmalahmidi@gmail.com>
2024-01-08 22:00:01 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = set()
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        visited.add(current_box)
+
+        for key in boxes[current_box]:
+            if key < n and key not in visited:
+                queue.append(key)
+
+    return len(visited) == n
