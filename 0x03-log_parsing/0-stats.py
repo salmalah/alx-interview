331e714a148fdb60b75f4cbaaf93fac82e3217a8
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-23 18:18:29 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
+'''This module is to define a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-def print_statistics(total_size, codes_status):
-    print("File size: {}".format(total_size))
-    for c in sorted(codes_status.keys()):
-        if codes_status[c] > 0:
-            print("{}: {}".format(c, codes_status[c]))
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_s = 0
+coun = 0
 
-def process_line(line, total_size, codes_status):
-    try:
-        parts = line.split()
-        if len(parts) == 9:
-            code_status = int(parts[7])
-            file_s = int(parts[8])
-            total_size += file_s
+try:
+    for l in sys.stdin:
+        line_list = l.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_s += size
+            coun += 1
 
-            if code_status in {200, 301, 400, 401, 403, 404, 405, 500}:
-                if code_status not in codes_status:
-                    codes_status[code_status] = 0
-                codes_status[code_status] += 1
+        if coun == 10:
+            coun= 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        return total_size, codes_status
-    except ValueError:
-        return total_size, codes_status
+except Exception as err:
+    pass
 
-def main():
-    total_size = 0
-    codes_status = {}
-
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            total_size, codes_status = process_line(line.strip(), total_size, codes_status)
-
-            if i % 10 == 0:
-                print_statistics(total_size, codes_status)
-    except KeyboardInterrupt:
-        pass
-
-    print_statistics(total_size, codes_status)
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_s))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
