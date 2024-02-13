2582ed8f7f49ea7268ca97d20a2508a88a067cae
salmalah <122815714+salmalah@users.noreply.github.com>
2024-02-13 05:35:53 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,35 @@
 #!/usr/bin/node
 
-const request = require('request');
-const movieId = parseInt(process.argv[2], 10);
+const util = require('util');
+const request = util.promisify(require('request'));
 
-request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, async (error, response, body) => {
-  if (error) return;
-  const movie = JSON.parse(body);
+if (process.argv.length !== 3 || isNaN(parseInt(process.argv[2]))) {
+  console.error('Usage: ./starWarsCharacters.js <Film_ID>');
+  process.exit(1);
+}
 
-  for (const character of movie.characters) {
-    const characterName = await new Promise((resolve, reject) => {
-      request.get(character, (error, response, body) => {
-        if (error) return;
-        const characterInfo = JSON.parse(body);
-        resolve(characterInfo.name);
-      });
-    });
-    console.log(characterName);
+const filmID = parseInt(process.argv[2]);
+
+async function starwarsCharacters (filmId) {
+  try {
+    const endpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+    const response = await request(endpoint);
+    const filmData = JSON.parse(response.body);
+
+    if (!filmData.characters || filmData.characters.length === 0) {
+      console.log('No characters found for the given film ID.');
+      return;
+    }
+
+    for (const urlCharacter of filmData.characters) {
+      const characterResponse = await request(urlCharacter);
+      const character = JSON.parse(characterResponse.body);
+      console.log(character.name);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
   }
-});
+}
+
+starwarsCharacters(filmID);
