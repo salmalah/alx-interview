4ec1db134990530541e124d1b2d9d187dd357a64
salmalah <122815714+salmalah@users.noreply.github.com>
2024-05-14 21:12:35 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-"""Prime game code"""
-
-
-def if_Prime(n):
-    """Checks if a number is prime."""
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def get_Prime(num):
-    """gets the prime numbers in a list."""
-    count = 0
-    for num in num:
-        if if_Prime(num):
-            count += 1
-    return count
+"""
+This module is to define isWinner"""
 
 
 def isWinner(x, nums):
-    """Determines who the winner of the game is."""
+    """
+    x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
     ben = 0
     maria = 0
-    if x <= 0 or not nums:
-        return None
-    for num in range(x):
-        num_arr = [n for n in range(1, nums[num] + 1)]
-        if get_Prime(num_arr) % 2 == 0:
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        fc_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
     if ben > maria:
         return "Ben"
-    if ben == maria:
-        return None
-    return "Maria"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def fc_multiples(ls, x):
+    """
+    removes multiple of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
